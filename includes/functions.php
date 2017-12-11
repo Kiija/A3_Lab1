c2dab5d2080374ccb9041e97a40f56e29f10fb98
Gargarello <k_gargarello@tss-hr420-dm40.fc.ca>
2017-12-11 17:44:31 -0500

    added videos and started thumbnail things

--- includes/functions.php
+++ includes/functions.php
@@ -42,4 +42,18 @@ if (isset($_GET['carModel'])) {
   echo json_encode($row);
 }
 
+if (isset($_GET['getVideos'])) {
+
+  $myQuery = "SELECT * FROM video";
+
+  $result = mysqli_query($conn, $myQuery);
+  $rows = array();
+
+  while ($row = mysqli_fetch_assoc($result)){
+    $rows[] = $row;
+  }
+
+  echo json_encode($row);
+}
+
  ?>
